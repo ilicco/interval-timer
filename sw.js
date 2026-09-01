930ca39fc2bd625491c409a7a9ede229fbb2ca08
Ilicco <ilicco@Iliccos-MacBook-Neo.local>
2026-09-01 20:09:47 +0100

    Slow down countdown speech slightly to fix 'Two' sounding like 'do'

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this when index.html or voice.html changes so the new version gets cached.
-var CACHE_NAME = "interval-timer-v4";
+var CACHE_NAME = "interval-timer-v5";
 var ASSETS = ["./", "./index.html", "./voice.html"];
 
 self.addEventListener("install", function (event) {
